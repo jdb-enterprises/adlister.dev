ecb4fb2db698bd9c3e951b4e0703b5ec21982ea4
Ben Roberts <mikebenroberts@gmail.com>
2016-10-04 11:39:20 -0500

    added jQuery and bootstrap.js CDNs

--- public/index.php
+++ public/index.php
@@ -20,11 +20,6 @@
 
     	<?php require '../views/partials/common_js.php'; ?>
 
-		<!-- Jquery.js CDN -->
-		<script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
-		<!-- Bootstrap.js CDN -->
-		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
 	</body>
 </html>
 
