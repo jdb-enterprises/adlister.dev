d543d8a60b12175a9fd318147e523b424dbe2f38
Ben Roberts <mikebenroberts@gmail.com>
2016-10-04 15:22:12 -0500

    user seeder

--- database/seeds/user_seeder.php
+++ database/seeds/user_seeder.php
@@ -7,6 +7,11 @@ $user->name = 'Finn Mertens';
 $user->email = 'finn@fansofbilly.com';
 $user->username = "finn_the_human";
 $user->password = $_ENV['USER_PASS'];
+$user->address = '123 BS Ave';
+$user->city = 'San Antonio';
+$user->zip_code = '78015';
+$user->state ='TX';
+
 $user->save();
 
 $user = new User;
@@ -14,6 +19,10 @@ $user->name = 'Jake';
 $user->email = 'jake@fansofbilly.com';
 $user->username = "jake_the_dog";
 $user->password = $_ENV['USER_PASS'];
+$user->address = '123 BS Ave';
+$user->city = 'San Antonio';
+$user->state ='TX';
+$user->zip_code = '78015';
 $user->save();
 
 $user = new User;
@@ -21,6 +30,10 @@ $user->name = 'Simon Petrikov';
 $user->email = 'iceking@princessaholicanonymous.com';
 $user->username = "ice_king";
 $user->password = $_ENV['USER_PASS'];
+$user->address = '123 BS Ave';
+$user->city = 'San Antonio';
+$user->state ='TX';
+$user->zip_code = '78015';
 $user->save();
 
 $user = new User;
@@ -28,4 +41,8 @@ $user->name = 'Marceline Abadeer';
 $user->email = 'marceline@vampirequeen.com';
 $user->username = "marceline_abadeer";
 $user->password = $_ENV['USER_PASS'];
+$user->address = '123 BS Ave';
+$user->city = 'San Antonio';
+$user->state ='TX';
+$user->zip_code = '78015';
 $user->save();
