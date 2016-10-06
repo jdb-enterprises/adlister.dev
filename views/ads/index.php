2a73b7eb7f949a815dd9272078b93451d9505f95
Ben Roberts <mikebenroberts@gmail.com>
2016-10-06 16:15:10 -0500

    fixed autopopulate on account edit page

--- views/ads/index.php
+++ views/ads/index.php
@@ -22,7 +22,7 @@ $allItems = $item->all();
             <div class="thumbnail">
                 <img class="imgSize" src="<?= $allItems->attributes[$i]['image'] ?> " alt="">
                 <div class="caption">
-                    <h4 class="pull-right"><?= $allItems->attributes[$i]['price'] ?></h4>
+                    <h4 class="pull-right">$<?= $allItems->attributes[$i]['price'] ?></h4>
                     <h4><a href="#"><?= $allItems->attributes[$i]['name'] ?></a>
                     </h4>
                     <p><?= $allItems->attributes[$i]['description'] ?><p>
