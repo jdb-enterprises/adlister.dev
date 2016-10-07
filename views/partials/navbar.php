27c60f37eb90fd81a02033bc9e36799604b1acaa
Ben Roberts <mikebenroberts@gmail.com>
2016-10-07 15:56:24 -0500

    navbar styling/fixed form control to allow lowercase letters

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -8,16 +8,15 @@
 		<span class="icon-bar"></span>
 		<span class="icon-bar"></span>
 	  </button>
-	  <a class="navbar-brand" href="/">Home</a>
-	  <a class="navbar-brand" href="/index">View All Items</a>
+	  <a class="navbar-brand active" href="/">Home</a>
 	</div>
 
 	<!-- Collect the nav links, forms, and other content for toggling -->
 	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 	  <ul class="nav navbar-nav">
-		<!-- <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+		<li><a href="/index">View All<span class="sr-only">(current)</span></a></li>
 		<li><a href="#">Link</a></li>
-		<li class="dropdown">
+		<!-- <li class="dropdown">
 		  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
 		  <ul class="dropdown-menu">
 			<li><a href="#">Action</a></li>
@@ -31,7 +30,7 @@
 		</li> -->
 	  </ul>
 
-	  <form action="/show" class="navbar-form navbar-left">
+	  <form action="/show" class="navbar-form navbar-right">
 		<div class="form-group">
 		  <input id="search" name="search" type="search" class="form-control" placeholder="Search">
 		</div>
