e75238cf25a294447ffbcb7cb22167ad5bc18b55
Ben Roberts <mikebenroberts@gmail.com>
2016-10-07 16:23:00 -0500

    fixed regex on signup page

--- views/home.php
+++ views/home.php
@@ -57,7 +57,7 @@ $allItems = $item->all();
                     <!-- forloop --> <?php for ($i=0; $i < 6; $i++) { ?>
     
          <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
+            <div class="box thumbnail">
                 <img class="imgSize" src="<?= $allItems->attributes[$i]['image'] ?> " alt="">
                 <div class="caption">
                     <h4 class="pull-right">$<?= $allItems->attributes[$i]['price'] ?></h4>
