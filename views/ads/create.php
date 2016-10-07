a5ea28446eb257f0f8d3fcd5a46864616a8add49
Ben Roberts <mikebenroberts@gmail.com>
2016-10-07 11:58:12 -0500

    fixed migration FK and added form control to create ad page

--- views/ads/create.php
+++ views/ads/create.php
@@ -2,68 +2,70 @@
 
 // Checks to see if the proper keys exist in the $_REQUEST, creates new instance of Item class, and saves new item
 if(isset($_REQUEST['price']) && isset($_REQUEST['name']) && isset($_REQUEST['description']) && isset($_FILES['image'])) {
-  $item = new Item;
-  $item->price = Input::get('price');
-  $item->name = Input::get('name');
-  $item->description = Input::get('description');
-  $item->image = saveUploadedImage('image');
-  $item->user_id = $_SESSION['LOGGED_IN_ID'];
-  
-  $item->save();
+	$item = new Item;
+	$item->price = Input::get('price');
+	$item->name = Input::get('name');
+	$item->description = Input::get('description');
+	$item->image = saveUploadedImage('image');
+	$item->user_id = $_SESSION['LOGGED_IN_ID'];
+	$_SESSION['SUCCESS_MESSAGE'] = 'AD Created Successfully';
+	
+	$item->save();
 }
 
 
  ?>
+
+<div class="container create-form">
+<div class="form-group row">
+ <?php if (isset($_SESSION['ERROR_MESSAGE'])) : ?>
+    <div class="alert alert-danger">
+        <p class="error"><?= $_SESSION['ERROR_MESSAGE']; ?></p>
+    </div>
+    <?php unset($_SESSION['ERROR_MESSAGE']); ?>
+<?php endif; ?>
+<?php if (isset($_SESSION['SUCCESS_MESSAGE'])) : ?>
+    <div class="alert alert-success">
+        <p class="success"><?= $_SESSION['SUCCESS_MESSAGE']; ?></p>
+    </div>
+    <?php unset($_SESSION['SUCCESS_MESSAGE']); ?>
+<?php endif; ?>
 <!--Page for creating new advertisement listings-->
 
-<form class="form-horizontal" method="post" enctype="multipart/form-data">
-<fieldset>
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label">Name</label>  
-  <div class="col-md-5">
-  <input id="name" name="name" type="text" placeholder="Name" class="form-control input-md" required="">
-    
+	<form class="form-horizontal " method="post" enctype="multipart/form-data">
+		
+			
+  <label for="example-text-input" pattern="([A-Z])\w+" class="col-xs-2 col-form-label">Name</label>
+  <div class="col-xs-6">
+    <input class="form-control" id="name" name="name" type="text" required>
   </div>
 </div>
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label">Price</label>  
-  <div class="col-md-5">
+<div class="form-group row">
+  <label for="price" class="col-xs-2 col-form-label">price</label>
+  <div class="col-xs-6">
     <div class="input-group">
-      <span class="input-group-addon">$</span>
-      <input id="price" name="price" type="text" class="form-control input-md">
-    </div>
+		<span class="input-group-addon">$</span>
+		<input id="price" pattern="[0-9]*\.[0-9]{2}" name="price" type="text" class="form-control input-md" required>
+	</div>
   </div>
 </div>
-
-<!-- Textarea -->
-<div class="form-group">
-  <label class="col-md-4 control-label">Description</label>
-  <div class="col-md-5">                     
-    <textarea class="form-control" id="description" name="description" placeholder="Description of item"></textarea>
+<div class="form-group row">
+  <label for="description" class="col-xs-2 col-form-label">Description</label>
+  <div class="col-xs-6">
+    <textarea id="description" type="text" name="description" class="form-control" required></textarea>
   </div>
 </div>
-
-<div class="form-group">
-  <label class="col-md-4 control-label">Upload</label>
-  <div class="col-md-5">                     
-     <input type="file" id="image" name="image" accept="image/*">
-
+<div class="form-group row">
+  <label for="example-url-input" class="col-xs-2 col-form-label">Uploaded Image</label>
+  <div class="col-xs-6">
+    <input class="form-control" type="file" name="image" id="image-input" accept="image/*" required>
+    <img class="img-thumbnail thumbnail" id="preview" src="#" alt="Uploaded Image">
   </div>
 </div>
+	<div class="form-group col-md-5 pull-left">                    
+		<input class="btn btn-primary" type="submit">
+	</div>
 
-<div class="form-group">
-  <label class="col-md-4 control-label"></label>
-    <div class="col-md-5">                     
-      <input class="btn btn-primary" type="submit">
-    </div>
+	
+	</form>
 </div>
-
-
-
-</fieldset>
-</form>
-
